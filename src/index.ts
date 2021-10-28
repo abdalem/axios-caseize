71a389cbf0a8e177313b4c6a582e6484b6d0e1bf
abdalem <abdennour@abbi-studio.fr>
2021-10-28 12:26:11 +0200

    caseize only json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import caseize, { CompatibleCasesConstants } from 'caseize';
-import { Cases, CaseizeInterceptors } from './types';
+import caseize, { CompatibleCasesConstants } from "caseize";
+import { Cases, CaseizeInterceptors } from "./types";
 
 const defaultCases: Cases = {
   request: CompatibleCasesConstants.SNAKECASE,
   response: CompatibleCasesConstants.CAMELCASE,
-}
+};
 
 const caseizeInterceptors: CaseizeInterceptors = (instance, cases = {}) => {
   cases = {
@@ -12,7 +12,7 @@ const caseizeInterceptors: CaseizeInterceptors = (instance, cases = {}) => {
     ...cases,
   };
 
-  instance.interceptors.request.use(req => {
+  instance.interceptors.request.use((req) => {
     if (cases.request) {
       if (req.data) req.data = caseize(req.data, cases.request);
       if (req.params) req.params = caseize(req.params, cases.request);
@@ -21,8 +21,15 @@ const caseizeInterceptors: CaseizeInterceptors = (instance, cases = {}) => {
     return req;
   });
 
-  instance.interceptors.response.use(res => {
-    if (res.data && cases.response) res.data = caseize(res.data, cases.response);
+  instance.interceptors.response.use((res) => {
+    if (res.data && cases.response) {
+      try {
+        JSON.parse(JSON.stringify(res.data));
+        res.data = caseize(res.data, cases.response);
+      } catch (error) {
+        console.error("data is not caseizable");
+      }
+    }
 
     return res;
   });
